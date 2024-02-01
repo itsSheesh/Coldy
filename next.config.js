743bf899c2089c77e427579be2112adf703ef55d
Mohamad amin <145129471+itsSheesh@users.noreply.github.com>
2024-02-01 17:47:18 +0330

    next config

--- next.config.js
+++ next.config.js
@@ -1,14 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: "export",
-    images: {
-        remotePatterns : [
-            {
-                protocol: "https",
-                hostname: "openweathermap.org",
-            }
-        ]
-    }
+    output: 'export',
+    // images: {
+    //     remotePatterns : [
+    //         {
+    //             protocol: "https",
+    //             hostname: "openweathermap.org",
+    //         }
+    //     ]
+    // }
 }
 
 module.exports = nextConfig
